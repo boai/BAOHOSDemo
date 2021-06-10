ee516b79fbc3efd14c4bfc71eaacd052bbb14a29
boai <137361770@qq.com>
2021-06-10 16:44:44 +0800

    feat：新增 按钮跳转页面demo

--- entry/src/main/java/com/example/myapplication1/slice/MainAbilitySlice.java
+++ entry/src/main/java/com/example/myapplication1/slice/MainAbilitySlice.java
@@ -4,12 +4,17 @@ import com.example.myapplication1.ResourceTable;
 import ohos.aafwk.ability.AbilitySlice;
 import ohos.aafwk.content.Intent;
 import ohos.global.resource.Resource;
+import ohos.agp.components.Button;
 
 public class MainAbilitySlice extends AbilitySlice {
     @Override
     public void onStart(Intent intent) {
         super.onStart(intent);
         super.setUIContent(ResourceTable.Layout_ability_main);// 加载XML布局
+
+        Button button = (Button) findComponentById(ResourceTable.Id_button);
+        // 点击按钮跳转至第二个页面
+        button.setClickedListener(listener -> present(new SecondAbilitySlice(), new Intent()));
     }
 
     @Override
